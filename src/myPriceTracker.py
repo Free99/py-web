647a30ffcb9de1df4f471492db57b3fcb6957182
free99 <tianshan.cui@gmail.com>
2017-02-14 22:34:25 -0800

    It works. :)

--- src/myPriceTracker.py
+++ src/myPriceTracker.py
@@ -1,8 +1,12 @@
 __author__ = 'Tianshan'
 
 import requests
+from bs4 import BeautifulSoup
 
 requests = requests.get("https://www.amazon.com/Logitech-Widescreen-Calling-Recording-Desktop/dp/B006JH8T3S/ref=sr_1_1?ie=UTF8&qid=1487139536&sr=8-1&keywords=logitech%2B920&th=1")
-# <span id="priceblock_ourprice" class="a-size-medium a-color-price">$59.30</span>
 
-print(requests.content)
+content = requests.content
+soup = BeautifulSoup(content, "html.parser")
+element = soup.find("span", {"id": "priceblock_ourprice", "class": "a-size-medium a-color-price"})
+
+print(element.text.strip())
