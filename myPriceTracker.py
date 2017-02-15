7d4ac4ad69765ec3a0341741d9e2786176374037
free99 <tianshan.cui@gmail.com>
2017-02-14 22:44:53 -0800

    Price tracker

--- myPriceTracker.py
+++ myPriceTracker.py
@@ -9,4 +9,7 @@ content = requests.content
 soup = BeautifulSoup(content, "html.parser")
 element = soup.find("span", {"id": "priceblock_ourprice", "class": "a-size-medium a-color-price"})
 
-print(element.text.strip())
+string_price = element.text.strip() # "$59.30"
+price_without_symbol = string_price[1:] # "59.30"
+
+print("The current price is {}.".format(string_price))
