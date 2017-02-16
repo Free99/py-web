dc6f092beebfc97a120c2d1d6c1f1be7ee61615d
free99 <tianshan.cui@gmail.com>
2017-02-15 23:03:15 -0800

    Modified and create post.json() function.

--- models/post.py
+++ models/post.py
@@ -2,7 +2,23 @@ __author__ = 'Tianshan'
 
 class Post(object):
 
-    def __init__(self, title, content, author):
+    def __init__(self, blog_id, title, content, author, date, id):
+        self.blog_id = blog_id
         self.title = title
         self.content = content
         self.author = author
+        self.created_date = date
+        self.id = id
+
+    def save_to_mongo(self):
+        pass
+
+    def json(self):
+        return {
+            'id': self.id,
+            'blog_id': self.blog_id,
+            'title': self.title,
+            'content': self.content,
+            'author': self.author,
+            'created_date': self.created_date
+        }
