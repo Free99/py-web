e68df26747159cfd9384392df76a2d6b7acfaaf2
free99 <tianshan.cui@gmail.com>
2017-02-28 23:53:47 -0800

    Try out the ninja, make some basic templates.

--- WebBlog/src/app.py
+++ WebBlog/src/app.py
@@ -1,12 +1,13 @@
+from flask import Flask, render_template
+
 __author__ = 'Tianshan'
 
-from flask import Flask
 
 app = Flask(__name__)  # '__main__'
 
 @app.route('/')
 def greatings():
-    return "Hello, world!"
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run() # Default port is 5000, use (port=XXXX) to change it
