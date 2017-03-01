5c4f651801bceec22b034c898f8fc05ad0602e47
free99 <tianshan.cui@gmail.com>
2017-02-28 23:40:07 -0800

    Create new_blog and new_post method.

--- WebBlog/src/models/user.py
+++ WebBlog/src/models/user.py
@@ -1,4 +1,6 @@
 import uuid
+
+import datetime
 from flask import session
 from WebBlog.src.common.database import Database
 from WebBlog.src.models.blog import Blog
@@ -12,28 +14,8 @@ class User(object):
         self._id = uuid.uuid4().hex if _id is None else _id
 
     @classmethod
-    def get_by_email(cls, email):
-        data = Database.find_one('users', {'email': email})
-        if data is not None:
-            return cls(**data)
-
-    @classmethod
-    def get_by_id(cls, _id):
-        data = Database.find_one('users', {'_id': _id})
-        if data is not None:
-            return cls(**data)
-
-    @staticmethod
-    def login_valid(email, password):
-        # Check whether email matches password
-        user = User.get_by_email(email)
-        if user is not None:
-            return user.password == password
-        return False
-
-    @classmethod
     def register(cls, email, password):
-        user = cls.get_by_email(email)
+        user = cls.get_user_by_email(email)
         if user is None:
             # User doesn't exist
             new_user = cls(email, password)
@@ -43,15 +25,6 @@ class User(object):
         else:
             # Exists
             return False
-
-    @staticmethod
-    def login(user_email):
-        # login_valid has already been called
-        session['email'] = user_email
-
-    @staticmethod
-    def logout():
-        session['email'] = None
 
     def get_blogs(self):
         return Blog.find_by_author_id(self._id)
@@ -65,3 +38,50 @@ class User(object):
 
     def save_to_mongo(self):
         Database.insert('users', self.json())
+
+    @classmethod
+    def get_user_by_email(cls, email):
+        data = Database.find_one('users', {'email': email})
+        if data is not None:
+            return cls(**data)
+        # return None by default
+
+    @classmethod
+    def get_user_by_id(cls, _id):
+        data = Database.find_one('users', {'_id': _id})
+        if data is not None:
+            return cls(**data)
+
+    @staticmethod
+    def login_valid_check(email, password):
+        # Check whether email matches password
+        user = User.get_user_by_email(email)
+        if user is not None:
+            return user.password == password
+        return False
+
+    @staticmethod
+    def login(user_email):
+        # login_valid has already been called
+        session['email'] = user_email
+
+    @staticmethod
+    def logout():
+        # Empty the email from session
+        session['email'] = None
+
+    def new_blog(self, title, description):
+        # Creat a new blog
+        blog = Blog(author=self.email,
+                    title=title,
+                    description=description,
+                    author_id=self._id)
+        blog.save_to_mongo()
+
+    @staticmethod
+    def new_post(blog_id, title, content, date=datetime.datetime.utcnow()):
+        # Write a new post
+        blog = Blog.from_mongo(blog_id)
+        blog.new_post(title=title,
+                      content=content,
+                      date=date)
